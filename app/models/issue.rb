7b8504bda9368fb619741b8c2193c97de61a5079
Jay Adkisson <jjmadkisson@gmail.com>
2011-06-18 14:16:58 -0700

    flesh out Issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,7 +2,10 @@ class Issue
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :text
+  field :title
+  field :description
+
+  validates_length_of :title, :in => 25..105, :allow_nil => false
 
   embeds_many :votes
 end
