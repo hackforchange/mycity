a2b63d50a50295f506aaef44f09d120dedfae5b3
Phill Kenoyer <phill@kenoyer.com>
2011-06-19 11:20:27 -0700

    Fixed the auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :token_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :votes
+
+  before_save :ensure_authentication_token
 end
