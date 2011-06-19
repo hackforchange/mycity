8fd1105ba3c70d43910159f2171e2efa1cc349da
Jay Adkisson <jjmadkisson@gmail.com>
2011-06-19 11:16:58 -0700

    403 if you've already voted on that issue

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -9,8 +9,11 @@ module Api
         vote_count = Issue.find(params[:issue_id]).votes.count
         render :json => { issue_id: @vote.issue_id, vote_count: vote_count }
       else
-        render :json => { errors: @vote.errors }, status: 403
+        render :json => { errors: "already voted on that issue" }, status: 403
       end
+    rescue ActiveRecord::StatementInvalid
+      render :json => { errors: @vote.errors }
     end
+
   end
 end
