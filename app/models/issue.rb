422c9eeb08d826b2ed89d1e8668ab53f7ca394b8
Phill Kenoyer <phill@kenoyer.com>
2011-06-19 05:55:09 -0700

    Added Long/Lat Added finders reformatted

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,18 +2,18 @@ class Issue
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  def self.recent
-    all.desc(:created_at)
-  end
-
-  def self.popular
-  end
-
   field :title
   field :description
   field :tag
+  field :longitude
+  field :latitude
 
   validates_length_of :title, :in => 5..105, :allow_nil => false
+
+  has_many :votes
+
+  index :title
+  index :tag
 
   def as_json(opts={})
     super.merge(
@@ -21,9 +21,37 @@ class Issue
     )
   end
 
-  has_many :votes
+  def self.recent
+    all.desc(:created_at)
+  end
 
-  index :title
-  index :tag
+  def self.popular
+    Vote.only(:issue_id).aggregate.collect do |v|
+      i = Issue.find(v['issue_id'])
+      {
+        id: i.id,
+        title: i.title,
+        description: i.description,
+        tag: i.tag,
+        vote_count: v['count']
+      }
+    end
+  end
+
+  def self.maintenence
+    tag(:maintenence)
+  end
+
+  def self.transportation
+    tag(:transportation)
+  end
+
+  def self.other
+    tag(:other)
+  end
+
+  def self.tag(tag)
+    where(tag: tag.to_str)
+  end
 
 end
