8c07d7c37a7013c434e22c0dd386e38db1743a33
Jay Adkisson <jjmadkisson@gmail.com>
2011-06-18 17:42:20 -0700

    require authentication on VotesController#create

--- app/controllers/api/issues/votes_controller.rb
+++ app/controllers/api/issues/votes_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module Issues
     class VotesController < ApplicationController
+      before_filter :authenticate_user!, :only => :create
+
       def create
         @vote = Vote.new(params[:vote])
 
